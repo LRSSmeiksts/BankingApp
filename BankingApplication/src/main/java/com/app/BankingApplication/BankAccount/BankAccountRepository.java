9a75599e02072d2f8ae7645252bd35ea48d30be0
LRSSmeiksts <131581606+LRSSmeiksts@users.noreply.github.com>
2024-04-05 15:09:26 +0300

    Delete BankingApplication directory

--- BankingApplication/src/main/java/com/app/BankingApplication/BankAccount/BankAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.BankingApplication.BankAccount;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
-    BankAccount findByUsername(String username);
-}
